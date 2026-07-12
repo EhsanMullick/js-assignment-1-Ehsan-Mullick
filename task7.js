ee7b172c76090190f720f3f6504465d905eea021
Rythm of Arees <83583425+EhsanMullick@users.noreply.github.com>
2026-07-12 14:35:11 +0600

    Create task7.js

---
+++ task7.js
@@ -0,0 +1,16 @@
+let myScore = 85;
+let friendScore = 65;
+if (myScore > 80) {
+    if (friendScore > 80) {
+        console.log("Go for a lunch.");
+    } else if (friendScore >= 60 && friendScore <= 80) {
+        console.log("Good luck next time.");
+    } else if (friendScore >= 40 && friendScore < 60) {
+        console.log("Keep friend's message unseen.");
+    } else {
+        console.log("Block your friend.");
+    }
+} else {
+    
+    console.log("Go to home and sleep and act sad.");
+}
